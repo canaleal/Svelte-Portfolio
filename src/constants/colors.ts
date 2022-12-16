f3416d22af168f1a3ea8bccd137335c9fae656b9
canaleal <alexcanales766@gmail.com>
2022-12-15 22:17:12 -0500

    Created some constants for colors globals and enviroments

---
+++ src/constants/colors.ts
@@ -0,0 +1,20 @@
+export const COLORS = [
+    'bg-red-500',
+    'bg-yellow-500',
+    'bg-green-500',
+    'bg-blue-500',
+    'bg-indigo-500',
+    'bg-purple-500',
+    'bg-pink-500',
+    'bg-gray-500',
+    'bg-orange-500',
+    'bg-red-400',
+    'bg-yellow-400',
+    'bg-green-400',
+    'bg-blue-400',
+    'bg-indigo-400',
+    'bg-purple-400',
+    'bg-pink-400',
+    'bg-gray-400',
+    'bg-black-400'
+];
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export * from './colors';
+export * from './environment';
+export * from './global';
