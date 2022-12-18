4538c12ffb900040df6ec9cee81faec11ae8fe34
canaleal <alexcanales766@gmail.com>
2022-12-18 16:47:04 -0500

    Created sorting utils

---
+++ src/utils/sorting-utils.ts
@@ -0,0 +1,13 @@
+
+
+export const sortAscending = (array: any[], key: string) => {
+    return array.sort((a, b) => {
+        if (a[key] < b[key]) {
+            return -1;
+        }
+        if (a[key] > b[key]) {
+            return 1;
+        }
+        return 0;
+    });
+}
