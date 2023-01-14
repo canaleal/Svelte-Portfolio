3b6d8379de6fc01af5e203280ff317bb0f00cfb0
canaleal <alexcanales766@gmail.com>
2023-01-13 19:37:33 -0500

    Moved head and app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,6 @@
 	import Navbar from '../components/Navbar.svelte';
 </script>
 
-<svelte:head>
-	<title>Alex Canales Portfolio</title>
-	<meta name="portfolio" content="noindex nofollow" />
-	<html lang="en" />
-	<link rel="stylesheet" href="/src/styles/style.css" />
-</svelte:head>
-
 <Navbar />
 <section class="main">
 	<slot />
