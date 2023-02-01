fe3e165e0421a487b54db2dda7aa0277f5bfc619
canaleal <alexcanales766@gmail.com>
2023-02-01 09:19:47 -0500

    Created sorting descending util

--- src/utils/sorting-utils.ts
+++ src/utils/sorting-utils.ts
@@ -10,3 +10,16 @@ export const sortAscending = (array: any[], key: string) => {
         return 0;
     });
 }
+
+export const sortDescending = (array: any[], key: string) => {
+
+    return array.sort((a, b) => {
+        if (a[key] > b[key]) {
+            return -1;
+        }
+        if (a[key] < b[key]) {
+            return 1;
+        }
+        return 0;
+    });
+};
