72bb2b59442da642653cf155071062e1c3bc85fb
Alex Canales <alexcanales766@gmail.com>
2023-12-03 12:28:01 -0500

    Added cursor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,12 @@
 
 	import { onMount } from 'svelte';
 
+	let x = 0;
+	let y = 0;
+	const updateMousePosition = (event: { clientX: number; clientY: number }) => {
+		x = event.clientX;
+		y = event.clientY;
+	};
 </script>
 
 <svelte:head>
@@ -16,14 +22,16 @@
 	<html lang="en" />
 </svelte:head>
 
+<svelte:window on:mousemove={updateMousePosition} />
+
 <section class="flex flex-row px-64 py-32">
 	<div class="fixed h-screen">
 		<Header />
 	</div>
-
 	<div class="flex-1 " />
-
 	<main class="flex-1 flex flex-col gap-16">
 		<slot />
 	</main>
 </section>
+
+<div class="cursor-circle" style="left: {x}px; top: {y}px;" />
