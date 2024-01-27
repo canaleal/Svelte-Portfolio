2ad15ab3f448858b39593e1e1bc5a0a6c249672b
Alex Canales <alexcanales766@gmail.com>
2024-01-27 04:11:02 -0500

    Updated about

--- src/lib/features/about/about.svelte
+++ src/lib/features/about/about.svelte
@@ -3,13 +3,13 @@
 
 <section id="About" class="p-4">
 	<p>
-		I'm a <span class="text-teal-300">full-Stack software developer</span> with a knack for crafting
-		robust, user-centric applications. I've worked with a variety of technologies and frameworks,
-		from
-		<span class="text-teal-300">Machine Learning</span> to
-		<span class="text-teal-300">Web Development</span>, and even some
-		<span class="text-teal-300">UI Design and 3D Modelling</span> I'm always looking to expand my skillset
-		and try new things.
+		I'm a <span class="text-teal-300">full-stack software developer</span> who loves building solid,
+		user-friendly applications. I've dabbled in everything from
+		<span class="text-teal-300">Machine Learning</span>
+		to <span class="text-teal-300">Web Development</span>, and even some
+		<span class="text-teal-300">UI Design and 3D Modelling</span> I'm always up for learning new
+		stuff and expanding my toolkit. On the side, I'm into <span class="text-teal-300">game development</span>, using my coding skills
+		to whip up some cool and fun gaming experiences 
 	</p>
 	<p class="mt-4">
 		I enjoy keeping a pulse on the latest tech trends, tinkering with gadgets, and occasionally
