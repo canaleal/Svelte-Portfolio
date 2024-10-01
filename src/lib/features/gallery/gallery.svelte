018e03c2edb474743d55235100b9a46bf3b183e7
Alex Canales <alexcanales766@gmail.com>
2024-10-01 02:20:53 -0400

    Added images

--- src/lib/features/gallery/gallery.svelte
+++ src/lib/features/gallery/gallery.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   const EXAMPLE_IMAGES: string[] = [
-    '/assets/images/halo/halo.webp'
+    '/assets/images/halo/halo.webp',
+    '/assets/images/gallery/fighter.webp',
+    '/assets/images/gallery/gaea.webp',
 ]
 
 </script>
 
-<section id="Gallery" class="grid grid-cols-1 gap-8 px-8 py-12">
+<section id="Gallery" class="flex flex-col lg:flex-row gap-8 px-8 py-12">
   {#each EXAMPLE_IMAGES as image}
-    <img src={image} alt="" class="special-card__image special-card__image--xl shadow" />
+    <img src={image} alt="" class="special-card__image special-card__image--xl fade-in" />
   {/each}
 </section>
