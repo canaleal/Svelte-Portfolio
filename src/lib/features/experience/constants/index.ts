a803b9906b0e89c08a321459be159e932d113630
Alex Canales <alexcanales766@gmail.com>
2024-10-06 06:11:13 -0400

    reduce exp

--- src/lib/features/experience/constants/index.ts
+++ src/lib/features/experience/constants/index.ts
@@ -6,7 +6,7 @@ export const EXPERIENCE: IExperience[] = [
     company: 'Geotab',
     position: 'Full Stack Software Developer ',
     description:
-      'Our team maintained MyGeotab, a comprehensive fleet management solution, and successfully updated it to React for improved performance and usability. We also built an LLM extension that is now used by 2,000 employees, enhancing productivity and operations. Additionally, we managed over 150 million records for 700+ partner companies, ensuring data integrity and accessibility.',
+      'Our team maintained MyGeotab, a comprehensive fleet management solution, and successfully updated it to React for improved performance and usability. We also built an LLM extension that is now used by 2,000 employees, enhancing productivity and operations.',
     startDate: 'Dec 2022',
     endDate: 'Present',
     technologies: ['React', 'NextJS', 'Nodejs', 'Gitlab', 'dotnetcore', 'Azure', 'Docker', 'Googlecloud']
